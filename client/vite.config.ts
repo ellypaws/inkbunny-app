5351e8fa3e306f20db2e89a191751554585df208
Elly <145195760+ellypaws@users.noreply.github.com>
2024-03-07 06:22:05 +0800

    Serve 1323 for api in React app as /api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from "path";
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:1323',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 });

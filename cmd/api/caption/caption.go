ef7997eedd4d098dbce26d44bce60eb36975cf1e
Elly <145195760+ellypaws@users.noreply.github.com>
2024-04-09 18:37:28 +0800

    Reduce cache hit message

--- cmd/api/caption/caption.go
+++ cmd/api/caption/caption.go
@@ -40,7 +40,6 @@ func ProcessCaption(c echo.Context, wg *sync.WaitGroup, sub *db.Submission, i in
 
 	item, err := cacheToUse.Get(c, key)
 	if err == nil {
-		c.Logger().Infof("Cache hit for %s", f.FileURLScreen)
 		var result *e.CaptionEnum
 		err := json.Unmarshal(item.Blob, &result)
 		if err != nil {

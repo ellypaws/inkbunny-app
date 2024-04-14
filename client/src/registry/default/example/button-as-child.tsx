ea8555f78cd15867e5df04c7df1f0a6445d479fe
Elly <145195760+ellypaws@users.noreply.github.com>
2024-04-15 07:32:39 +0800

    Remove the entirety of /client to move to a different branch for now

--- client/src/registry/default/example/button-as-child.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/registry/default/ui/button"
-
-export default function ButtonAsChild() {
-  return (
-    <Button asChild>
-      <a href="/login">Login</a>
-    </Button>
-  )
-}
--- client/src/registry/new-york/example/button-as-child.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/registry/new-york/ui/button"
-
-export default function ButtonAsChild() {
-  return (
-    <Button asChild>
-      <a href="/login">Login</a>
-    </Button>
-  )
-}

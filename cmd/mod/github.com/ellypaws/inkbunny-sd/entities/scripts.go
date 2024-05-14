5cf3f50b8be3fb0573a4c0cf47b0867df273f867
Elly <145195760+ellypaws@users.noreply.github.com>
2024-05-14 23:19:13 +0800

    chore: temporarily remove inkbunny-sd to move as a git submodule

--- cmd/mod/github.com/ellypaws/inkbunny-sd/entities/scripts.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type Scripts struct {
-	ADetailer  *ADetailer  `json:"ADetailer,omitempty"`
-	ControlNet *ControlNet `json:"ControlNet,omitempty"`
-	CFGRescale *CFGRescale `json:"CFG Rescale Extension,omitempty"`
-}

bcaa8441bce94c8544601d515da6260fbe80666a
Elly <145195760+ellypaws@users.noreply.github.com>
2024-07-21 19:02:23 +0800

    style: use config.Output instead of reading from QueryParam

--- cmd/api/service/detail.go
+++ cmd/api/service/detail.go
@@ -85,7 +85,7 @@ func ProcessResponse(c echo.Context, config *Config) []Detail {
 func processSubmission(c echo.Context, submission *api.Submission, config *Config, detail *Detail) {
 	defer config.wg.Done()
 
-	sub := InkbunnySubmissionToDBSubmission(*submission, c.QueryParam("output") == OutputReportIDs)
+	sub := InkbunnySubmissionToDBSubmission(*submission, config.Output == OutputReportIDs)
 
 	if sub.Metadata.AISubmission {
 		c.Logger().Infof("processing files for %s %s", sub.URL, sub.Title)
